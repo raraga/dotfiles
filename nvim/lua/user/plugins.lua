30bdc394fb4b399fa14639627ab11b869e129a93
Renato Araga <raraga@gmail.com>
2022-11-13 20:15:53 -0800

    added intelephense and lsp

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -25,8 +25,83 @@ use 'tjdevries/colorbuddy.nvim'
 use 'tpope/vim-commentary'
 use 'jiangmiao/auto-pairs'
 
+use 'hrsh7th/cmp-nvim-lsp'
+use 'hrsh7th/cmp-buffer'
+use 'hrsh7th/cmp-path'
+use 'hrsh7th/cmp-cmdline'
+use 'hrsh7th/nvim-cmp'
+use 'L3MON4D3/LuaSnip'
+use 'saadparwaiz1/cmp_luasnip'
+
+vim.opt.completeopt={"menu", "menuone", "noselect"}
+
+-- Set up nvim-cmp.
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    mapping = {
+      ["<C-j>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<C-k>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
 -- Solarized Dark configured by Color Buddy
 require('user.plugins.neosolarized').setup()
+
+require'lspconfig'.intelephense.setup{
+    capabilities = capabilities,
+    on_attach = function()
+    local keymap = vim.keymap.set
+        keymap('n', 'K', vim.lsp.buf.hover, {buffer=0})
+        keymap('n', 'gd', vim.lsp.buf.definition, {buffer=0})
+        keymap('n', '<leader>t', vim.lsp.buf.type_definition, {buffer=0})
+        keymap('n', 'gi', vim.lsp.buf.type_definition, {buffer=0})
+        keymap('n', '<leader>gj', vim.diagnostic.goto_next, {buffer=0})
+        keymap('n', '<leader>gk', vim.diagnostic.goto_prev, {buffer=0})
+        keymap('n', '<leader>gr', vim.lsp.buf.rename, {buffer=0})
+        keymap('n', '<leader>Td', '<cmd>Telescope diagnostics<cr>', {buffer=0})
+        keymap('n', 'glf', '<cmd>Telescope lsp_references<cr>', {buffer=0})
+    end,
+}
 
 use({
   'nvim-telescope/telescope.nvim', tag = '0.1.0',
